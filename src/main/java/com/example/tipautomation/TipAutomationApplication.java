338e47547c46a4828bcfce083ee51147260bf1f5
Michael Morrison <157526447+MikeMorrison1996@users.noreply.github.com>
2026-03-04 15:14:03 -0500

    First commit making the project

---
+++ src/main/java/com/example/tipautomation/TipAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tipautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TipAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tipautomation/TipAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tipautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
